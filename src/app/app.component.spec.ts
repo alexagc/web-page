32c26c8e65020829c7f3912faecfc1591a74dbe9
Alejandro Gomez Canal <alex@ip-192-168-1-34.eu-west-1.compute.internal>
2018-09-02 23:13:56 +0200

    FEAT(): update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,25 +3,28 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'web-page'`, async(() => {
+
+  it(`should have as title 'Work in progress!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('web-page');
+    expect(app.title).toEqual('Work in progress');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-page!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Work in progress!'
+    );
   }));
 });
